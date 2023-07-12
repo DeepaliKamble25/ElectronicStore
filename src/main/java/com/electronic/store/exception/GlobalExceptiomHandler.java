c3eb741bb35fa4b72b2f89990782f8fa641dcc4f
DeepaliKamble25 <kambledeepali25@gmail.com>
2023-07-12 12:03:52 +0530

    MethodArgumentNotValidException implemented in GlobalExceptiomHandler  done

--- src/main/java/com/electronic/store/exception/GlobalExceptiomHandler.java
+++ src/main/java/com/electronic/store/exception/GlobalExceptiomHandler.java
@@ -2,19 +2,29 @@ package com.electronic.store.exception;
 
 
 import com.electronic.store.playload.ApiResponse;
-import org.hibernate.hql.internal.classic.AbstractParameterInformation;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptiomHandler {
 
     public  Logger logger= LoggerFactory.getLogger(GlobalExceptiomHandler.class);
-   @ExceptionHandler
+
+
+    @ExceptionHandler
     public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException ex){
 
      logger.info("Exception  Handler  invoked");
@@ -24,4 +34,30 @@ public class GlobalExceptiomHandler {
 
 
    }
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse> userNotFoundException(UserNotFoundException ex){
+
+        logger.info("User Exception  Handler  invoked");
+        ApiResponse response = ApiResponse.builder().message(ex.getMessage()).status(HttpStatus.NOT_FOUND).success(true).build();
+
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND) ;
+
+
+    }
+//    MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleMethodeArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response=new HashMap<>();
+        allErrors.stream().forEach(objectError ->
+        {
+            String defaultMessage = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+            response.put(field,defaultMessage);
+        });
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+
 }
