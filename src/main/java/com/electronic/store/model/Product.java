e86bd6f965bce56727e6fab94e6f8f083d23a2ea
DeepaliKamble25 <kambledeepali25@gmail.com>
2023-07-21 12:29:03 +0530

    in model product field added

--- src/main/java/com/electronic/store/model/Product.java
+++ src/main/java/com/electronic/store/model/Product.java
@@ -13,40 +13,40 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name="products")
+@Table(name = "products")
 public class Product {
 
     @Id
-    private  String productId;
-    @Column(name="title")
+    private String productId;
+
+    @Column(name = "title")
     private String title;
-    @Column(length=1000)
+
+    @Column(length = 1000)
     private String description;
 
-    @Column(name="price")
+    @Column(name = "price")
     private int price;
-    @Column(name="Discount_Price")
+
+    @Column(name = "Discounted_Price")
     private int discountedPrice;
-    @Column(name="quantity")
+
+    @Column(name = "quantity")
     private int quantity;
-    @Column(name="Date")
+
+    @Column(name = "added_date")
     private Date addeddate;
 
     @Column(name = "live")
     private boolean live;
+
     @Column(name = "Stock")
     private boolean stock;
 
-
     private String productImageName;
-    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "category_product_Id")
     private Category category;
-
-
-
-
-
 
 
 }
