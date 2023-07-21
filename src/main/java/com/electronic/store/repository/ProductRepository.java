ce8b7a901039cf73f4d6b1a7675e208fe8978491
DeepaliKamble25 <kambledeepali25@gmail.com>
2023-07-21 12:31:19 +0530

    product repository implementation done

--- src/main/java/com/electronic/store/repository/ProductRepository.java
+++ src/main/java/com/electronic/store/repository/ProductRepository.java
@@ -13,7 +13,7 @@ public interface ProductRepository extends JpaRepository<Product,String> {
 
 
     Page<Product> findByTitleContaining(String subTitle,Pageable pageable);
-    Page<Product> findByLiveTrue(Pageable pageable );
+    Page<Product> findByLiveTrue(Pageable pageable);
 
    Page<Product> findByCategory(Category category,Pageable pageable);
 }
