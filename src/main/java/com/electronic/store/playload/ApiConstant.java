ca3b7e408cb20c4f57625f1a8b3e6b2c60300187
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-07-22 21:33:51 +0530

    constant added in api constant  class

--- src/main/java/com/electronic/store/playload/ApiConstant.java
+++ src/main/java/com/electronic/store/playload/ApiConstant.java
@@ -6,8 +6,8 @@ public class ApiConstant {
     public static String User_DELETED="User Deleted Successfully !!!";
 
     public static String Bad_ApiRequest_Message="Bad Request !!!";
-    public static String Bad_ApiRequest1="File with this :";
-    public static String Bad_ApiRequest2="not allowed !!!";
+    public static String Bad_ApiRequest="File with give extension is not allowed !!!";
+
 
     public static String FILE_NOT_FOUND_EXP="File not  found !!!";
 
