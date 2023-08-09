03eb5f7993e79ec1dad8c7af371c0b2714dfc099
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-09 16:58:37 +0530

    implemente BaseEntity with help of auditing

--- src/main/java/com/electronic/store/model/BaseEntity.java
+++ src/main/java/com/electronic/store/model/BaseEntity.java
@@ -1,13 +1,31 @@
 package com.electronic.store.model;
 
-import javax.persistence.Embeddable;
-import java.util.Date;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Embeddable
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @CreatedBy
     private String createdBy;
+    @LastModifiedBy
     private String updatedBy;
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
+    @LastModifiedDate
     private Date updatedDate;
 }
