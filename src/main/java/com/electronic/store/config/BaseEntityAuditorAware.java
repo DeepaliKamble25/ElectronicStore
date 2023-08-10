82166ab94899d8a98884d66231d3dc4dd53b399f
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-10 08:24:58 +0530

    BaseEntityAuditorAware implemente logger

---
+++ src/main/java/com/electronic/store/config/BaseEntityAuditorAware.java
@@ -0,0 +1,23 @@
+package com.electronic.store.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class BaseEntityAuditorAware implements AuditorAware<String> {
+    /**
+     * Returns the current auditor of the application.
+     *
+     * @return the current auditor.
+     */
+    public static Logger logger= LoggerFactory.getLogger(BaseEntityAuditorAware.class);
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        logger.info("Initiating request to getCurrentAuditor" );
+
+        return Optional.ofNullable("Deepali Kamble").filter(s->s.isEmpty());
+    }
+}
