b631e25939c54f17cec0cbdfb7b4fecfe835b9e6
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-10 13:45:08 +0530

    CartItemRepository implementation done

--- src/main/java/com/electronic/store/repository/CartItemRepository.java
+++ src/main/java/com/electronic/store/repository/CartItemRepository.java
@@ -1,4 +1,7 @@
 package com.electronic.store.repository;
 
-public class CartItemRepository {
+import com.electronic.store.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository <CartItem,Integer>{
 }
