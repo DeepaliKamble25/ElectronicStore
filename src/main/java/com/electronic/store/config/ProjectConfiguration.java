2e46921f839f04c7c43c64138c73939103598b90
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-10 08:28:16 +0530

    In ProjectConfiguration AuditAware bean is created and EnableJpaAuditing annoted  on class

--- src/main/java/com/electronic/store/config/ProjectConfiguration.java
+++ src/main/java/com/electronic/store/config/ProjectConfiguration.java
@@ -3,13 +3,21 @@ package com.electronic.store.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ProjectConfiguration {
 
     @Bean //class is created to inject
     public ModelMapper modelMapper() {
 
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new BaseEntityAuditorAware();
     }
 }
