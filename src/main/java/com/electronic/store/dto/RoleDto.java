6ba3313ec49a474e59a549e5495b676207d6ba8c
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-21 15:27:23 +0530

    RoleDto implementation done

---
+++ src/main/java/com/electronic/store/dto/RoleDto.java
@@ -0,0 +1,16 @@
+package com.electronic.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+
+    private String roleId;
+    private String roleName;
+}
