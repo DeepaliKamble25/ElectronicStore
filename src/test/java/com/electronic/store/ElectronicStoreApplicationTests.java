bc50966aa1eed121686850e2b4ac7156da93457e
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-27 22:33:55 +0530

    main file implementatio of commandLineRunner.

--- src/test/java/com/electronic/store/ElectronicStoreApplicationTests.java
+++ src/test/java/com/electronic/store/ElectronicStoreApplicationTests.java
@@ -1,13 +1,24 @@
 package com.electronic.store;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ElectronicStoreApplicationTests {
+class ElectronicStoreApplicationTests implements CommandLineRunner {
 
 	@Test
 	void contextLoads() {
 	}
 
+	/**
+	 * Callback used to run the bean.
+	 *
+	 * @param args incoming main method arguments
+	 * @throws Exception on error
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
