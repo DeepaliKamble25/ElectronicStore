7a1b0bd8c23efea54bee498925fce5429384bd67
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-28 07:55:50 +0530

    SecurityConfig implementation done

---
+++ src/main/java/com/electronic/store/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.electronic.store.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+
+
+}
