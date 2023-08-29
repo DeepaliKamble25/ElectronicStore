5bc27353c1cc059b060afd154d558a0b6b9a1d44
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-08-29 21:17:54 +0530

    JwtAuthenticationEntryPoint class implementation done

---
+++ src/main/java/com/electronic/store/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.electronic.store.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static Logger logger= LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+
+            logger.error("Unauthorized error: {}", authException.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            PrintWriter writer = response.getWriter();
+            writer.println("Access Denied !!! "+ authException.getMessage());
+
+    }
+}
